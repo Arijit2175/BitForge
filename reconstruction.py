8a3345c115af14a3fc0b3beb22e955fdba885458
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-13 21:06:35 +0530

    changes made

--- reconstruction.py
+++ reconstruction.py
@@ -1,3 +1,5 @@
+import os
+
 def reconstruct_file(file_name, chunk_size, total_chunks, output_file_path):
     with open(output_file_path, 'wb') as output_file:
         for chunk_index in range(total_chunks):
