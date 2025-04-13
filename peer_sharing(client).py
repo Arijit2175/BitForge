0d8eca51a4c9ce800ea367248018585742220ec2
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-13 19:51:19 +0530

    connecting to server

--- peer_sharing(client).py
+++ peer_sharing(client).py
@@ -1,3 +1,6 @@
 import socket
 import os 
 
+def client(peer_ip, peer_port, chunk_index, torrent_metadata):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((peer_ip, peer_port))
