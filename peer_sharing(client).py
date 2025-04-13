5ca8d35a3b1e947bf72adaf2ccdee6d10da4d43c
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-13 19:51:54 +0530

    requesting chunks

--- peer_sharing(client).py
+++ peer_sharing(client).py
@@ -4,3 +4,10 @@ import os
 def client(peer_ip, peer_port, chunk_index, torrent_metadata):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((peer_ip, peer_port))
+
+    chunk_hashes = eval(client_socket.recv(1024).decode())
+    print(f"Available chunks: {chunk_hashes}")
+
+
+    print(f"Requesting chunk {chunk_index}")
+    client_socket.send(str(chunk_index).encode())
