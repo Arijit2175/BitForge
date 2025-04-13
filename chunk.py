e0ae2f7893c7daa13979ef0565fa046e5c681106
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-13 15:25:58 +0530

    adding file operations

--- chunk.py
+++ chunk.py
@@ -4,3 +4,9 @@ import json
 
 Chunk_size = 1024 * 1024
 
+def chunk_file(file_path):
+    file_size = os.path.getsize(file_path)
+    file_name = os.path.basename(file_path)
+    chunk_hashes = []
+
+    
