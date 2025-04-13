2971e049c421091d4a8bb902cffe6607759be54e
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-13 15:28:56 +0530

    creating metadata to describe file

--- chunk.py
+++ chunk.py
@@ -13,3 +13,10 @@ def chunk_file(file_path):
         while chunk := f.read(Chunk_size):
             hash_obj = hashlib.sha256(chunk)
             chunk.hashes.append(hash_obj.hexdigest())
+
+    metadata = {
+        "file_name": file_name,         # Name of the original file
+        "file_size": file_size,         # Size in bytes
+        "chunk_size": CHUNK_SIZE,       # Size of each chunk
+        "chunk_hashes": chunk_hashes    # List of hashes for each chunk
+    }
