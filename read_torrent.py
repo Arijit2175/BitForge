74bdbd9e3d717e2ecbe6cc85d599272bf57317d4
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-13 15:43:32 +0530

    print content to verify data

--- read_torrent.py
+++ read_torrent.py
@@ -3,3 +3,8 @@ import json
 def read_torrent_file(torrent_file_path):
     with open(torrent_file_path, "r") as file:
         metadata = json.load(file)
+
+    print(f"Loaded metadata from {torrent_file_path}:")
+    print(json.dumps(metadata, indent=4))  
+
+    return metadata
