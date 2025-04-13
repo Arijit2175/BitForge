637ed1da5795e97705b7f35f0c873e96853a71d8
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-13 19:52:23 +0530

    receive and save chunk data

--- peer_sharing(client).py
+++ peer_sharing(client).py
@@ -11,3 +11,11 @@ def client(peer_ip, peer_port, chunk_index, torrent_metadata):
 
     print(f"Requesting chunk {chunk_index}")
     client_socket.send(str(chunk_index).encode())
+
+    chunk_data = client_socket.recv(torrent_metadata['chunk_size'])
+    
+    with open(f"chunk_{chunk_index}_{torrent_metadata['file_name']}", "wb") as f:
+        f.write(chunk_data)
+
+    print(f"Chunk {chunk_index} downloaded successfully!")
+    client_socket.close()
