80bbec391cf8516f5110e6f6d294f3c8f944ecad
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-13 19:43:49 +0530

    setting up server

--- peer_sharing(server).py
+++ peer_sharing(server).py
@@ -1,3 +1,8 @@
 import socket 
 import os
 
+def server(peer_port, torrent_metadata):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('0.0.0.0', peer_port))  
+    server_socket.listen(5)
+    print(f"Server listening on port {peer_port}...")
