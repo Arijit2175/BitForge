645880f72d3fc8c2e11ee7b1311e5c721f9ed358
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-15 20:32:37 +0530

    changed

--- register_to_tracker.py
+++ register_to_tracker.py
@@ -12,7 +12,7 @@ def register_with_tracker(tracker_ip, tracker_port, file_name, ip, port, availab
     try:
         response = requests.post(url, json=data)
         if response.status_code == 200:
-            print("✅ Successfully registered with tracker:", response.json())
+            print("Successfully registered with tracker:", response.json())
         else:
             print("Failed to register. Tracker responded with:", response.status_code, response.text)
     except Exception as e:
