ba76536e5721f585483b5de950b284a90d672988
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-15 19:15:16 +0530

    registering peers

--- tracker.py
+++ tracker.py
@@ -1,3 +1,17 @@
 import json
 import socket
 import threading
+
+peer_chunk_map = {}
+
+def handle_peer(conn, addr):
+    try:
+        data = conn.recv(4096).decode()
+        request = json.loads(data)
+
+        if request["type"] == "register":
+            peer = (request["ip"], request["port"])
+            chunks = request["chunks"]
+            peer_chunk_map[peer] = chunks
+            print(f"Registered peer {peer} with chunks {chunks}")
+            conn.send(b"registered")
