600bb43af61b69ebb5b53c51e7cf7008973194fd
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-15 19:25:59 +0530

    function for getting peers

--- get_peers_from_tracker.py
+++ get_peers_from_tracker.py
@@ -1,1 +1,13 @@
 import requests
+
+def get_peers_for_chunk(tracker_url, file_name, chunk_index):
+    params = {
+        "file_name": file_name,
+        "chunk_index": chunk_index
+    }
+    response = requests.get(f"{tracker_url}/get_peers", params=params)
+    if response.status_code == 200:
+        return response.json().get("peers", [])
+    else:
+        print("Failed to get peers from tracker:", response.text)
+        return []
