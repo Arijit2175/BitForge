08348d9131d3ef65ae5b7043df510e79898d3cad
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-15 20:40:37 +0530

    changed for peer registration

--- client.py
+++ client.py
@@ -1,12 +1,31 @@
 import socket
 import os
 import json
+import requests
 from urllib.parse import urlparse 
 from read_torrent import read_torrent_file
 from download import download_chunk 
 from reconstruction import reconstruct_file  
 from verification import verify_file 
 from get_peers_from_tracker import get_peers_for_chunk
+
+def register_with_tracker(tracker_ip, tracker_port, file_name, ip, port, available_chunks):
+    url = f"http://{tracker_ip}:{tracker_port}/register"
+    data = {
+        "file_name": file_name,
+        "ip": ip,
+        "port": port,
+        "chunks": available_chunks
+    }
+
+    try:
+        response = requests.post(url, json=data)
+        if response.status_code == 200:
+            print("Successfully registered with tracker:", response.json())
+        else:
+            print("Failed to register. Tracker responded with:", response.status_code, response.text)
+    except Exception as e:
+        print("Error during registration:", e)
 
 def client(tracker_ip, tracker_port, torrent_metadata):
     chunk_hashes = torrent_metadata['chunk_hashes']
