3f5375e5ccd2ed691d6239f976b9fb264b9c4ae3
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-15 19:53:24 +0530

    modified the retrieval of peers

--- get_peers_from_tracker.py
+++ get_peers_from_tracker.py
@@ -13,13 +13,20 @@ def get_peers_for_chunk(tracker_ip, tracker_port, file_name, chunk_index):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         try:
             s.connect((tracker_ip, tracker_port))  
-            s.send(message.encode())  
+            s.send(message.encode()) 
 
-            response = s.recv(4096).decode()
-            peers = json.loads(response)
+            response = s.recv(4096).decode()  
+            peers = json.loads(response) 
 
-            return peers
+            if isinstance(peers, list):
+                return peers  
+            else:
+                print("Unexpected response format from tracker:", peers)
+                return []
+
+        except json.JSONDecodeError:
+            print("Error decoding JSON response from tracker.")
+            return []
         except Exception as e:
             print(f"Error connecting to tracker: {e}")
             return []
-
