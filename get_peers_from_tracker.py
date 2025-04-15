041fd5a73a046c8ea34aed4d2412a2ed20c98d85
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-15 19:42:01 +0530

    modified getting peers to socket

--- get_peers_from_tracker.py
+++ get_peers_from_tracker.py
@@ -1,13 +1,25 @@
-import requests
+import socket
+import json
 
-def get_peers_for_chunk(tracker_url, file_name, chunk_index):
-    params = {
+def get_peers_for_chunk(tracker_ip, tracker_port, file_name, chunk_index):
+    data = {
+        "type": "lookup",
         "file_name": file_name,
         "chunk_index": chunk_index
     }
-    response = requests.get(f"{tracker_url}/get_peers", params=params)
-    if response.status_code == 200:
-        return response.json().get("peers", [])
-    else:
-        print("Failed to get peers from tracker:", response.text)
-        return []
+
+    message = json.dumps(data)
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.connect((tracker_ip, tracker_port))  
+            s.send(message.encode())  
+
+            response = s.recv(4096).decode()
+            peers = json.loads(response)
+
+            return peers
+        except Exception as e:
+            print(f"Error connecting to tracker: {e}")
+            return []
+
