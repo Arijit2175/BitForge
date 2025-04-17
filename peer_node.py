a64521a5cf7f5b30b4e9078d7fbeff583c914bac
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-17 17:14:14 +0530

    for starting peer server

--- peer_node.py
+++ peer_node.py
@@ -4,3 +4,6 @@ from read_torrent import read_torrent_file
 from my_server import server
 from parallel_downloader import download_file
 from register_to_tracker import register_with_tracker
+
+def start_peer_server(peer_port, torrent_metadata):
+    server(peer_port, torrent_metadata)
