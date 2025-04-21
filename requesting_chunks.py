7a363859f1cd106687458cdb72ecca7935fc8759
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-04-21 19:15:49 +0530

    functional logic

--- requesting_chunks.py
+++ requesting_chunks.py
@@ -1,1 +1,8 @@
 import socket
+
+def request_chunk(peer_ip, peer_port, chunk_index):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as peer_socket:
+        peer_socket.connect((peer_ip, peer_port))
+        peer_socket.send(f"GET_CHUNK {chunk_index}".encode())
+        chunk_data = peer_socket.recv(1048576) 
+        return chunk_data
