596e0df2ad490c108e015b6a464527f3ea04e2c9
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-05-05 16:15:32 +0530

    finding available chunks

--- automated_seeder.py
+++ automated_seeder.py
@@ -7,3 +7,16 @@ from read_torrent import read_torrent_file
 from register_seeder import register_seeder_to_tracker  # You already have this
 from upload_chunks import seeding_server
 
+SEEDING_FOLDER = "seeding_folder"
+SEEDER_IP = "127.0.0.1"
+SEEDER_PORT = 5000
+TRACKER_IP = "127.0.0.1"
+TRACKER_PORT = 9000
+
+def find_available_chunks(file_name, total_chunks, output_dir="."):
+    available = []
+    for i in range(total_chunks):
+        chunk_path = os.path.join(output_dir, f"chunk_{i}_{file_name}")
+        if os.path.exists(chunk_path):
+            available.append(i)
+    return available
