2b79d4584f388212b8359eacb4ab34524711a300
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-05-07 20:08:30 +0530

    changed for valid bencoding

--- chunk_hash.py
+++ chunk_hash.py
@@ -1,8 +1,8 @@
 import os
 import hashlib
-import json
+import bencodepy
 
-Chunk_size = 1024 * 1024
+Chunk_size = 1024 * 1024 
 
 def chunk_file(file_path):
     file_size = os.path.getsize(file_path)
@@ -12,18 +12,23 @@ def chunk_file(file_path):
     with open(file_path, 'rb') as f:
         while chunk := f.read(Chunk_size):
             hash_obj = hashlib.sha256(chunk)
-            chunk_hashes.append(hash_obj.hexdigest())
+            chunk_hashes.append(hash_obj.digest()) 
 
-    metadata = {
-        "file_name": file_name,         
-        "file_size": file_size,         
-        "chunk_size": Chunk_size,       
-        "chunk_hashes": chunk_hashes   
+    info = {
+        'length': file_size,
+        'name': file_name,
+        'piece length': Chunk_size,
+        'pieces': b''.join(chunk_hashes), 
     }
 
-    torrent_file = file_path + ".torrent"
-    with open(torrent_file, "w") as out:
-        json.dump(metadata, out, indent=4)
+    torrent_data = {
+        'announce': 'http://127.0.0.1:9000',  
+        'info': info
+    }
+
+    torrent_file = file_path + '.torrent'
+    with open(torrent_file, 'wb') as f:
+        f.write(bencodepy.encode(torrent_data))
 
     print(f"Created .torrent file: {torrent_file}")
 
