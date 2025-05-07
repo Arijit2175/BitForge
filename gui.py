6634e1f966f587eada3db27562c007ea22618a2c
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-05-07 19:25:10 +0530

    for progress bar

--- gui.py
+++ gui.py
@@ -3,3 +3,14 @@ import time
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QProgressBar, QFileDialog, QLabel, QStatusBar
 from PyQt5.QtCore import QThread, pyqtSignal
 
+class DownloadThread(QThread):
+    update_progress = pyqtSignal(int) 
+    update_status = pyqtSignal(str)   
+
+    def __init__(self, file_size):
+        super().__init__()
+        self.file_size = file_size
+        self.downloaded = 0
+        self.running = False
+
+    
