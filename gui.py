952be1b5c17fc53eaf3fdc422b50d1feb32cfc71
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-05-07 19:24:30 +0530

    importing modules

---
+++ gui.py
@@ -0,0 +1,5 @@
+import sys
+import time
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QProgressBar, QFileDialog, QLabel, QStatusBar
+from PyQt5.QtCore import QThread, pyqtSignal
+
