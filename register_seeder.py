165c830827a33145ab9f732e0264fa42e093e063
Arijit2175 <arijitkarmakar2175@gmail.com>
2025-05-08 15:40:09 +0530

    modified the code

--- register_seeder.py
+++ register_seeder.py
@@ -1,14 +1,25 @@
 import requests
 import json
 
-def register_seeder_to_tracker(tracker_ip, tracker_port, file_name, peer_ip, peer_port, chunks):
+def register_seeder_to_tracker(tracker_ip, tracker_port, file_name, peer_ip, peer_port, chunk_hashes):
+    """
+    Registers the seeder with the tracker, sending file info and chunk hashes.
+
+    Parameters:
+    - tracker_ip: IP address of the tracker
+    - tracker_port: Port of the tracker
+    - file_name: Name of the file being seeded
+    - peer_ip: IP address of the seeder
+    - peer_port: Port number of the seeder
+    - chunk_hashes: List of SHA256 hashes for the chunks being seeded
+    """
     url = f"http://{tracker_ip}:{tracker_port}/register"
     
     data = {
-        "file_name": file_name,
-        "ip": peer_ip,
-        "port": peer_port,
-        "chunks": chunks
+        "file_name": file_name,  
+        "ip": peer_ip,           
+        "port": peer_port,       
+        "chunks": chunk_hashes   
     }
 
     headers = {'Content-Type': 'application/json'}
